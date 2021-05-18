841818261431937a780416d8999a82e42f10477f
Memphis83 <kiran.iyer83@gmail.com>
2021-05-18 17:46:37 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,66 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
-function App() {
-  const [count, setCount] = useState(0)
+const  App = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+      // useeffect
+      useEffect(() => {
+        filterHandler();
+        saveLocalTodos();
+      }, [todos, status]);
+
+  const filterHandler = () => {
+    switch(status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredToDos(todos.filter((todo) => todo.completed === false));
+        break;
+      default: 
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+  // save local todos
+  const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos))  
+  }
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]))
+    }else{ 
+     let todoLocal = JSON.parse(localStorage.getItem("todos"));
+     setTodos(todoLocal);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React! I'm kieran</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <header>
+      <h1>My Todo List</h1>
+    </header>
+    <Form 
+      todos={todos}
+      inputText={inputText}
+      setTodos={setTodos}
+      setInputText={setInputText}
+      setStatus={setStatus}
+      />
+    <TodoList setTodos={setTodos} todos={todos}  filteredTodos={filteredTodos}/>
     </div>
   )
 }
 
-export default App
+export default App;
